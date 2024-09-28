683a0f686cca9c07df12f26ecc6c8a302f45a27e
smasr07 <a.sobirjonov@newuu.uz>
2024-09-28 13:22:33 +0500

    Create problem4.cpp

---
+++ problem4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    int n, x;
+    cin >> n >> x;
+    if (n <= 2) cout << 1;
+    else cout << (n - 2 + 2 * x - 1) / x;
+    return 0;
+}
